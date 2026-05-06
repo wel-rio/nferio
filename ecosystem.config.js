d33c2b7d5a32153ce63fd08c1a3905c68f7e4a7d
Welington <welington.lourenco.soares@gmail.com>
2026-05-06 14:24:38 -0300

    fix: adicionado xvfb-run ao pm2 para compatibilidade com acbrlib

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
   apps: [
     {
       name: 'nferio-backend',
-      script: 'npm',
-      args: 'run dev', // Ou 'start' se você já estiver usando a pasta /dist
+      script: 'xvfb-run',
+      args: '--auto-servernum --server-args="-screen 0 1024x768x24" npm run dev',
       cwd: './backend',
       env: {
         NODE_ENV: 'production',
@@ -13,7 +13,7 @@ module.exports = {
     {
       name: 'nferio-frontend',
       script: 'npm',
-      args: 'run dev', // Ou 'start' se estiver usando o build
+      args: 'run dev',
       cwd: './frontend',
       env: {
         NODE_ENV: 'production',
