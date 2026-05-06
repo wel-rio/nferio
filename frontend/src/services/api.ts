f10e0a5849fb650c7ffede8c94a2e59169aca89a
Welington <welington.lourenco.soares@gmail.com>
2026-05-06 16:54:25 -0300

    fix: apontando frontend para o IP do VPS

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // URL do Backend - Altere para a URL da Cloudflare quando publicar
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3333/api';
+const API_URL = import.meta.env.VITE_API_URL || 'http://147.15.107.54:3000/api';
 
 const api = axios.create({
   baseURL: API_URL,
