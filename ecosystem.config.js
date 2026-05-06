5a95d693aa5a682ba3aebe636200321d62b12e84
Welington <welington.lourenco.soares@gmail.com>
2026-05-06 17:26:10 -0300

    fix: corrigido interpretador do PM2 para o servico fiscal

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,7 @@ module.exports = {
       script: 'xvfb-run',
       args: '--auto-servernum --server-args="-screen 0 1024x768x24" npx ts-node src/fiscal-server.ts',
       cwd: './backend',
+      interpreter: 'none',
       env: {
         NODE_ENV: 'production',
         PORT: 3000
